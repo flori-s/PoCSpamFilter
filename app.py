8901b0aa77be0ca0b31e2756b70d193b1c4d6ece
flori-s <150121780+flori-s@users.noreply.github.com>
2024-11-15 00:11:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from os import environ
+from os 
 from flask import Flask, render_template, request
 from Model import predict_spam, accuracy
 
@@ -34,5 +34,5 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
